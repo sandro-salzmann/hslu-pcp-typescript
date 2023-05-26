8a3ea382c3f33798f7da92ec1ad6e6ed73771f1f
Salzmann Sandro ssalzman <sandro.salzmann@stud.hslu.ch>
2023-05-26 10:31:20 +0200

    add remark on how js is single-threaded

--- exercise-week9-java34-ex4.ts
+++ exercise-week9-java34-ex4.ts
@@ -50,3 +50,35 @@ async function sleep(ms: number) {
 }
 
 export {};
+
+// // But pay attention: Javascript is single-threaded! It uses one thread to run all code.
+// (async () => {
+//   const longBlockingTask = async () => {
+//     let y = 0;
+//     for (let i = 0; i < 2_500_000_000; i++) {
+//       y += 1;
+//       if (y % 1_000_000 === 0) {
+//         process.stdout.write('.');
+//       }
+//     }
+//   };
+//   const shortTask = async () =>
+//     new Promise<void>((resolve) =>
+//       setTimeout(() => {
+//         console.log('Sort Task finished');
+//         resolve();
+//       }, 2000),
+//     );
+
+//   await Promise.all([shortTask(), longBlockingTask()]);
+// })();
+// // 'Sort Task finished' doesn't print after 2 seconds because longBlockingTask has a long "CPU-Bound Blocking" synchronous code.
+// // Only through "asynchronous non-blocking I/O" (async-await/Promises + event loop) the one thread can do other things instead of synchronously waiting for events.
+// // => That's why waiting for timeouts and intervals (and network etc.) doesn't block the entire app.
+
+// // Remember: Async functions are only "really" async if ...
+// //  - they call either async functions
+// //  - or perform an async I/O operation
+// //  - or perform synchronous code (CPU-Bound Blocking) on another thread (Worker Threads, e.g. Node Worker on Node.JS or Service Worker on Web)
+
+// // More here: https://betterprogramming.pub/why-just-using-async-await-and-promises-doesnt-make-your-code-asynchronous-e8ee9014d92e
